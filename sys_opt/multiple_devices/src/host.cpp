24a52730d5070b64d047fd4de0bfab3738efe595
Rajesh <46475847+rajeshkrmeena@users.noreply.github.com>
2022-03-17 10:27:40 +0530

    Multiple Device Data Exchange
    Exchanging data between multiple devices using CMAC of the device.

--- sys_opt/multiple_devices/src/host.cpp
+++ sys_opt/multiple_devices/src/host.cpp
@@ -1,21 +1,17 @@
-/**
-* Copyright (C) 2019-2021 Xilinx, Inc
-*
-* Licensed under the Apache License, Version 2.0 (the "License"). You may
-* not use this file except in compliance with the License. A copy of the
-* License is located at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-* License for the specific language governing permissions and limitations
-* under the License.
-*/
+#include "cmdlineparser.h"
+#include <iostream>
+#include <cstring>
+#include "ap_int.h"
 
-#include "xcl2.hpp"
+// XRT includes
+#include <xrt/xrt_bo.h>
+#include <xrt/xrt_device.h>
+#include <xrt/xrt_kernel.h>
+#include <xrt/xrt_uuid.h>
 
+//#include "../../../include/xclhal2.h"
+#include <xrt.h>
+//#include <xrt.h>
 #include <array>
 #include <map>
 #include <stdio.h>
@@ -23,136 +19,180 @@
 #include <string.h>
 #include <unistd.h>
 #include <vector>
-
+using namespace std;
 using std::array;
 using std::map;
 using std::vector;
 
-cl::Program load_cl2_binary(cl::Program::Binaries, cl::Device device, cl::Context context);
-// This example demonstrates how to split work among multiple devices.
+#define DATA_SIZE 1000
+#define DWIDTH 512
+#define DEST_0 0
+#define DEST_1 1
+
+
+
 int main(int argc, char** argv) {
-    if (argc != 3) {
-        std::cout << "Usage: " << argv[0] << " <XCLBIN File>" << " <XCLBIN File>" << std::endl;  // for two different board or xclbin 
+    // Command Line Parser
+    sda::utils::CmdLineParser parser;
+
+    // Switches
+    //**************//"<Full Arg>",  "<Short Arg>", "<Description>", "<Default>"
+    parser.addSwitch("--xclbin_file", "-x", "input binary file string", "");
+    parser.addSwitch("--xclbin_file2", "-x2", "input binary file string", "");
+
+    parser.addSwitch("--device_id", "-d", "device index", "1");
+    parser.parse(argc, argv);
+
+
+    vector<std::string> binaryFile(2);   
+    binaryFile[0] = parser.value("xclbin_file");
+    binaryFile[1] = parser.value("xclbin_file2");
+  
+   //int device_index = stoi(parser.value("device_id"));
+
+
+    if (argc < 5) {
+        parser.printHelp();
         return EXIT_FAILURE;
     }
 
-    auto binaryFile = argv[1];
-    auto binaryFile2 = argv[2];
-    cl_int err = CL_SUCCESS;
 
-    // OPENCL HOST CODE AREA START
-    // get_xil_devices() is a utility API which will find the Xilinx
-    // platforms and will return list of devices connected to Xilinx platform
-    auto devices = xcl::get_xil_devices();
-    auto device_count = devices.size();
 
-    static const int elements_per_device = xcl::is_hw_emulation() ? (1 << 10) : (1 << 20);
-    static const int elements = elements_per_device * device_count;
+    // Read settings
+ 
+   auto device_count = xclProbe(); 
+   std::cout << "Devices found on System : " << device_count << std::endl;
+  
 
-    vector<int, aligned_allocator<int> > A(elements, 32);
-    vector<int, aligned_allocator<int> > B(elements, 10);
-    vector<int, aligned_allocator<int> > C(elements);
 
-    // One element per device
-    vector<cl::Context> contexts(device_count);
-    vector<cl::Program> programs(device_count);
-    vector<cl::Kernel> kernels(device_count);
-    vector<cl::CommandQueue> queues(device_count);
-    vector<std::string> device_name(device_count);
+   // TODO : Device Name //
+/*   xclDeviceHandle handle; 
+   struct xclDeviceInfo2 *info;
+   const char* unused1; enum xclVerbosityLevel unused2;
 
-    vector<cl::Buffer> buffer_a(device_count);
-    vector<cl::Buffer> buffer_b(device_count);
-    vector<cl::Buffer> buffer_result(device_count);
-    vector<cl::Program::Binaries> bins(device_count);
-    vector<cl::Platform> platform;
-    std::vector<unsigned char> fileBuf[device_count];
-    OCL_CHECK(err, err = cl::Platform::get(&platform));
+for(int d=0; d<device_count; ++d)
+  {
 
-    size_t size_per_device = elements_per_device * sizeof(int);
-    static const int iter = xcl::is_hw_emulation() ? 2 : 10 * 1024;
-    size_t total_size = iter * size_per_device * device_count * 3;
-    std::string size_str = xcl::convert_size(total_size);
+     handle = xclOpen(d,unused1,unused2);
+     info = xclDeviceInfo2();
+     xclGetDeviceInfo2(handle, info);
+     std::cout << "error : " << std::endl;
+    // std::cout << "Devices: " << info->mMagic << std::endl;
+  
+  }
+*/
 
-    cl_context_properties props[3] = {CL_CONTEXT_PLATFORM, (cl_context_properties)(platform[0])(), 0};
-    std::cout << "Initializing OpenCL objects" << std::endl;
-    for (int d = 0; d < (int)device_count; d++) {
-        // In this example. We will create a context for each of the devices
-        std::cout << "Creating Context[" << d << "]..." << std::endl;
-        OCL_CHECK(err, contexts[d] = cl::Context(devices[d], props, nullptr, nullptr, &err));
-        OCL_CHECK(err, queues[d] = cl::CommandQueue(contexts[d], devices[d], CL_QUEUE_PROFILING_ENABLE, &err));
-        OCL_CHECK(err, device_name[d] = devices[d].getInfo<CL_DEVICE_NAME>(&err));
 
-        // read_binary_file() ia a utility API which will load the binaryFile
-        // and will return pointer to file buffer.
-        fileBuf[d] = ((d==0) ? xcl::read_binary_file(binaryFile) : xcl::read_binary_file(binaryFile2)) ; // Only for two different boards
-        bins[d].push_back({fileBuf[d].data(), fileBuf[d].size()});
-        programs[d] = load_cl2_binary(bins[d], devices[d], contexts[d]);
-        OCL_CHECK(err, kernels[d] = cl::Kernel(programs[d], "vadd", &err));
 
-        // Allocate Buffers in Global Memory
-        // Buffers are allocated using CL_MEM_USE_HOST_PTR for efficient memory and
-        // Device-to-host communication
-        size_t offset = d * elements_per_device;
-        std::cout << "Creating Buffers[" << d << "]..." << std::endl;
-        OCL_CHECK(err, buffer_a[d] = cl::Buffer(contexts[d], CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, size_per_device,
-                                                &A[offset], &err));
-        OCL_CHECK(err, buffer_b[d] = cl::Buffer(contexts[d], CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, size_per_device,
-                                                &B[offset], &err));
-        OCL_CHECK(err, buffer_result[d] = cl::Buffer(contexts[d], CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY,
-                                                     size_per_device, &C[offset], &err));
-    }
+   vector<xrt::device> devices(device_count);
+   vector<xrt::uuid> uuids(device_count);
+   
+  vector<xrt::kernel> krnl_mm2s(device_count);
+  vector<xrt::kernel> krnl_s2mm(device_count);
+  
+  vector<xrt::bo> bo_in(device_count);
+  vector<xrt::bo> bo_out(device_count);
 
-    std::chrono::high_resolution_clock::time_point TimeStart = std::chrono::high_resolution_clock::now();
-    for (int d = 0; d < (int)device_count; d++) {
-        OCL_CHECK(err, err = kernels[d].setArg(0, buffer_result[d]));
-        OCL_CHECK(err, err = kernels[d].setArg(1, buffer_a[d]));
-        OCL_CHECK(err, err = kernels[d].setArg(2, buffer_b[d]));
-        OCL_CHECK(err, err = kernels[d].setArg(3, elements_per_device));
-        OCL_CHECK(err, err = kernels[d].setArg(4, iter));
+  vector<xrt::run> run_tx(device_count);
+  vector<xrt::run> run_rx(device_count);
 
-        // Copy input data to device global memory
-        std::cout << "Copying data..." << std::endl;
-        OCL_CHECK(err, err = queues[d].enqueueMigrateMemObjects({buffer_a[d], buffer_b[d]}, 0 /*0 means from host*/));
 
-        // Launch the Kernel
-        std::cout << "Launching Kernel..." << std::endl;
-        OCL_CHECK(err, err = queues[d].enqueueTask(kernels[d]));
+    size_t vector_size_bytes = sizeof(ap_uint<DWIDTH>) * DATA_SIZE;
 
-        // Copy Result from Device Global Memory to Host Local Memory
-        std::cout << "Getting Results..." << std::endl;
-        OCL_CHECK(err, err = queues[d].enqueueMigrateMemObjects({buffer_result[d]}, CL_MIGRATE_MEM_OBJECT_HOST));
-    }
+vector < ap_uint<DWIDTH>* > bo_in_map(device_count);
+vector < ap_uint<DWIDTH>* > bo_out_map(device_count);
 
-    int dev = 0;
-    for (auto queue : queues) {
-        std::cout << "Waiting for work to finish on device " << dev++ << std::endl;
-        OCL_CHECK(err, err = queue.flush());
-        OCL_CHECK(err, err = queue.finish());
-    }
 
-    std::chrono::high_resolution_clock::time_point TimeEnd = std::chrono::high_resolution_clock::now();
-    double duration_in_ms = std::chrono::duration_cast<std::chrono::microseconds>(TimeEnd - TimeStart).count();
+  for(int d=0; d<device_count; ++d) 
+  {
+     std::cout << "Open the device " << d << std::endl;
+      devices[d] = xrt::device(d);
+     std::cout << "Load the xclbin " << binaryFile[d] << std::endl;
+      uuids[d] = devices[d].load_xclbin(binaryFile[d]);    
+  }
 
-    // OPENCL HOST CODE AREA ENDS
-    bool match = true;
-    for (int i = 0; i < elements; i++) {
-        int host_result = A[i] + B[i];
-        if (C[i] != host_result) {
-            std::cout << "Error: Result mismatch" << std::endl;
-            std::cout << "i = " << i << " CPU result = " << host_result << " Device result = " << C[i] << std::endl;
-            match = false;
-            break;
-        }
-    }
-    std::cout << "Total Size : " << size_str << std::endl;
-    std::cout << "Time Taken : " << duration_in_ms / 1000000 << "sec" << std::endl;
-    std::cout << "TEST " << (match ? "PASSED" : "FAILED") << std::endl;
-    return (match ? EXIT_SUCCESS : EXIT_FAILURE);
-}
+  for(int d=0; d<device_count; ++d) 
+  {
 
-cl::Program load_cl2_binary(cl::Program::Binaries bins, cl::Device device, cl::Context context) {
-    cl_int err;
-    std::vector<cl::Device> devices(1, device);
-    OCL_CHECK(err, cl::Program program(context, devices, bins, nullptr, &err));
-    return program;
+    krnl_mm2s[d] = xrt::kernel(devices[d], uuids[d], "krnl_mm2s_0");
+    krnl_s2mm[d] = xrt::kernel(devices[d], uuids[d], "krnl_s2mm_0");
+
+    std::cout << "Allocate Buffer in Global Memory for device " <<d<<endl;
+    bo_in[d]= xrt::bo(devices[d], vector_size_bytes, krnl_mm2s[d].group_id(0));
+    bo_out[d] = xrt::bo(devices[d], vector_size_bytes, krnl_s2mm[d].group_id(0));
+   
+    // Map the contents of the buffer object into host memory
+
+	bo_in_map[d] = bo_in[d].map<ap_uint<DWIDTH>*>();
+	bo_out_map[d] = bo_out[d].map<ap_uint<DWIDTH>*>();
+   
+    std::cout << "Creating Data for device " <<d<<endl;;
+
+    // Create the test data
+    for (ap_uint<DWIDTH> i = 0; i < DATA_SIZE; ++i)
+        bo_in_map[d][i] = d*i+1;
+
+    // Synchronize buffer content with device side
+       std::cout << "synchronize input buffer data to device global memory for 0\n";
+
+       bo_in[d].sync(XCL_BO_SYNC_BO_TO_DEVICE);
+
+  }
+
+ 
+
+  for(int d=0; d<device_count; ++d) 
+  {
+    std::cout << "Execution of the kernel for device" <<d<<endl;
+
+    run_tx[d] = xrt::run(krnl_mm2s[d]);
+    run_tx[d].set_arg(0, bo_in[d]);
+    run_tx[d].set_arg(2, vector_size_bytes);
+    run_tx[d].set_arg(3, DEST_0);
+    run_tx[d].start();
+ }
+
+
+//---------------------------------------------
+
+  for(int d=0; d<device_count; ++d) 
+  {
+    run_rx[d] = xrt::run(krnl_s2mm[d]);
+    run_rx[d].set_arg(0, bo_out[d]);
+    run_rx[d].set_arg(2, vector_size_bytes);
+    run_rx[d].start();
+  }
+
+
+  std::cout << "waiting for tx data"<<endl;
+
+  for(int d=0; d<device_count; ++d) 
+  {
+    run_tx[d].wait();
+  }
+
+ std::cout << "waiting for rx data"<<endl;
+  for(int d=0; d<device_count; ++d) 
+  {
+    run_rx[d].wait();
+  }
+
+    // Get the output;
+
+  for(int d=0; d<device_count; ++d) 
+  {
+    std::cout << "Get the output data from the device 0 " << std::endl;
+    bo_out[d].sync(XCL_BO_SYNC_BO_FROM_DEVICE);
+
+   for(ap_uint<DWIDTH> i=0;i<DATA_SIZE;i++)
+      std::cout<<"Sent="<<bo_in_map[d][i]<<"\t\t"<<"Received="<<bo_out_map[d][i]<<std::endl;
+  }
+ 
+  for(int d=0,d2=device_count - 1; d<device_count; ++d, --d2 ) 
+  {
+    if (std::memcmp(bo_out_map[d], bo_in_map[d2], DATA_SIZE))
+        throw std::runtime_error("Value read back does not match reference"); 
+   }
+    std::cout << "TEST PASSED\n";
+    return 0;
 }
